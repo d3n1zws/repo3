f599070fccb207cd94e792beb48b80561821eaf0
d3n1zws <danizeh@code.edu.az>
2026-09-11 20:11:00 +0300

    added task3

---
+++ MUSIC PLAYER SYSTEM/MusicPlayer.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MUSIC_PLAYER_SYSTEM
+{
+    internal class MusicPlayer
+    {
+        public string Brand { get; set; }
+        public string Model { get; set; }
+        public string CurrentSong { get; set; }
+        public bool IsPlaying { get; set; }
+        public int BatteryPercent { get; set; }
+        public int Volume {  get; set; }
+        public MusicPlayer(string brand, string model, int volume, string currentSong, bool isPlaying, int batteryPercent)
+        {
+            Brand = brand;
+            Model = model;
+            Volume = volume;
+            CurrentSong = currentSong;
+            IsPlaying = isPlaying;
+            BatteryPercent = batteryPercent;
+        }
+        public void Play()
+        {
+            if (CurrentSong == null)
+            {
+                Console.WriteLine("Mahni yoxdur");
+                return;
+            }
+            if (IsPlaying)
+                Console.WriteLine("Song is already playing!");
+            IsPlaying = true;
+        }
+        public void Pause()
+        {
+            if (CurrentSong == null)
+            {
+                Console.WriteLine("Mahni yoxdur");
+                return;
+            }
+            if (!IsPlaying)
+                Console.WriteLine("Song is already paused!");
+            IsPlaying = false;
+        }
+        public void IncreaseVolume()
+        {
+            Volume = Math.Min(Volume + 1, 100);
+        }
+        public void DecreaseVolume()
+        {
+            Volume = Math.Max(Volume - 1, 0);
+        }
+        public void ChangeSong(string songName)
+        {
+            CurrentSong = songName;
+        }
+        public void ShowInfo()
+        {
+            Console.WriteLine($"Brand: {Brand}, Model: {Model}, CurrentSong: {CurrentSong}, Is Playing: {IsPlaying}, BatteryPercent: {BatteryPercent}, Volume: {Volume}");
+        }
+        public override string ToString()
+        {
+            return $"Brand: {Brand}, Model: {Model}, CurrentSong: {CurrentSong}, Is Playing: {IsPlaying}, BatteryPercent: {BatteryPercent}, Volume: {Volume}";
+        }
+    }
+}
---
+++ MUSIC PLAYER SYSTEM/Program.cs
@@ -0,0 +1,35 @@
+﻿//MENYU
+
+//0 - Player haqqında məlumat almaq üçün
+//1 - Play
+//2 - Pause
+//3 - Səsi artırmaq
+//4 - Səsi azaltmaq
+//5 - Mahnını dəyişmək
+//6 - Proqramdan çıxmaq
+
+//---
+using MUSIC_PLAYER_SYSTEM;
+MusicPlayer player = new MusicPlayer("brand", "model", 99, "almaarmud", true, 85);
+while (true)
+{
+    int x = int.Parse(Console.ReadLine());
+    if (x == 0)
+        player.ShowInfo();
+    if (x == 1)
+        player.Play();
+    if (x == 2)
+        player.Pause();
+    if (x == 3)
+        player.IncreaseVolume();
+    if (x == 4)
+        player.DecreaseVolume();
+    if (x == 5)
+    {
+        Console.WriteLine("istediyiniz mahnini secin");
+        string s = Console.ReadLine();
+        player.ChangeSong(s);
+    }
+    if (x == 6)
+        break;
+}
